d98d18326afc7afad9e6c83c97228f13263db043
marcmata26 <marcmata26@gmail.com>
2023-09-12 16:37:59 +0000

    push completed file

--- kube-clusters.sh
+++ kube-clusters.sh
@@ -1,8 +1,43 @@
 #!/usr/bin/env bash
 
-# cat << EOF | tee /etc/modules-load.d/containerd.conf
-# overlay
-# br_netfilter
-# EOF
+# Run in Sudo
 
-echo hello-world
+cat << EOF | tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat << EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+apt-get update && apt-get install -y containerd
+
+mkdir -p /etc/containerd
+
+containerd config default | tee /etc/containerd/config.toml
+
+systemctl restart containerd
+swapoff -a
+apt-get update && apt-get install -y apt-transport-https curl
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+cat << EOF | tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+
+apt-get install -y kubelet=1.27.0-00 kubeadm=1.27.0-00 kubectl=1.27.0-00
+
+ apt-mark hold kubelet kubeadm kubectl
+
+ echo all-done
